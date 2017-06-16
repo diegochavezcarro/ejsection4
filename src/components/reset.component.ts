d8ae1b02218226d036edea6c692d9ec35443b626
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-16 00:46:43 -0300

    Fin assignment 2

---
+++ src/components/reset.component.ts
@@ -0,0 +1,47 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'app-reset',
+    template: `
+        <ion-row>
+            <ion-col>
+                <button
+                    ion-button
+                    color="danger"
+                    small
+                    block
+                    (click)="onReset('all')">
+                    Reset All
+                </button>
+            </ion-col>
+            <ion-col>
+                <button
+                    ion-button
+                    color="danger"
+                    small
+                    block
+                    (click)="onReset('tap')">
+                    Reset "Taps"
+                </button>
+            </ion-col>
+            <ion-col>
+                <button
+                    ion-button
+                    color="danger"
+                    small
+                    block
+                    (click)="onReset('press')">
+                    Reset "Presses"
+                </button>
+            </ion-col>
+        </ion-row>
+    `
+})
+
+export class ResetComponent {
+    constructor() { }
+    @Output() didReset = new EventEmitter<string>();
+    onReset(type:string){
+        this.didReset.emit(type);
+    }
+}
